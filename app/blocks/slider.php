a1eb37441364b096255c37afa65e82401a0923b4
Cristi Miu <cristi.miu@gmail.com>
2018-11-26 17:59:56 +0200

    revert back changes first page slider, css modif

--- app/blocks/slider.php
+++ app/blocks/slider.php
@@ -1,16 +1,15 @@
 <?php if (!isset($webpage)) die('Direct access not allowed');  ?>
 <!--Start Banner-->
-
-<section data-id="j1ix0hv" class="elementor-element elementor-element-j1ix0hv elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-settings="{&quot;shape_divider_top&quot;:&quot;&quot;,&quot;shape_divider_bottom&quot;:&quot;&quot;}" data-element_type="section">
+<section data-id="t1s774a" class="elementor-element elementor-element-t1s774a elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-settings="{&quot;shape_divider_top&quot;:&quot;&quot;,&quot;shape_divider_bottom&quot;:&quot;&quot;}" data-element_type="section">
 	<div class="elementor-container elementor-column-gap-default">
 		<div class="elementor-row">
-			<div data-id="50mw42j" class="elementor-element elementor-element-50mw42j elementor-column elementor-col-100 elementor-top-column" data-settings="[]" data-element_type="column">
+			<div data-id="kgb2xsf" class="elementor-element elementor-element-kgb2xsf elementor-column elementor-col-100 elementor-top-column" data-settings="[]" data-element_type="column">
 				<div class="elementor-column-wrap elementor-element-populated">
 					<div class="elementor-widget-wrap">
-						<div data-id="e8bj9oz" class="elementor-element elementor-element-e8bj9oz foliageblog-slider-normal foliageblog-slider-labels-none elementor-widget elementor-widget-image-carousel" data-settings="[]" data-element_type="image-carousel.default">
+						<div data-id="czukrvy" class="elementor-element elementor-element-czukrvy foliageblog-slider-pretty foliageblog-slider-labels-labels elementor-widget elementor-widget-image-carousel" data-settings="[]" data-element_type="image-carousel.default">
 							<div class="elementor-widget-container">
 								<div class="elementor-image-carousel-wrapper elementor-slick-slider" dir="ltr">
-									<div class="elementor-image-carousel slick-arrows-inside slick-dots-outside" data-slider_options='{&quot;slidesToShow&quot;:1,&quot;autoplaySpeed&quot;:5000,&quot;autoplay&quot;:true,&quot;infinite&quot;:true,&quot;pauseOnHover&quot;:true,&quot;speed&quot;:500,&quot;arrows&quot;:true,&quot;dots&quot;:true,&quot;rtl&quot;:false,&quot;fade&quot;:false}'>
+									<div class="elementor-image-carousel slick-image-stretch" data-slider_options='{&quot;slidesToShow&quot;:1,&quot;autoplaySpeed&quot;:5000,&quot;autoplay&quot;:true,&quot;infinite&quot;:true,&quot;pauseOnHover&quot;:true,&quot;speed&quot;:500,&quot;arrows&quot;:false,&quot;dots&quot;:false,&quot;rtl&quot;:false,&quot;fade&quot;:false}'>
                             <?php 
                                 if ($dataView->slider->rows != null)
                                 {
