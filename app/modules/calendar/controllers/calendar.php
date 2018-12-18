1e2a67a52f6b67e40398e464376007c189a80c84
Cristi Miu <cristi.miu@gmail.com>
2018-12-18 05:50:38 +0200

    revert connect to facebook graph events api

--- app/modules/calendar/controllers/calendar.php
+++ app/modules/calendar/controllers/calendar.php
@@ -4,9 +4,6 @@ class Calendar extends AdminController
 
 	function GetJsonData()
 	{
-	    $facebookevents = "https://graph.facebook.com/v3.2/326181691509639/events/created/?access_token=EAAEry03Wk4EBAPDGUiIQZCsJnzn0RZAJeolzncaP71DQH7ZBuRUUflsLq0QK9j4lTN4YZBotay5J18O1ZCsH6KoGZBE3THom6NrAgwndc3r8yzwak4uK9LXz5sYgHxdako9UZCdm3xiEpQjXax5Ol1KNt1ucms7CEDn1RghhMPRLaRZCX7pzyyXJmjs2X3SvyXiQabZBBHicY0RmLQIPRVH5apsWWEoMEZCCg2RsVEYbfUIQZDZDhttps://graph.facebook.com/v3.2/326181691509639/events/created/?access_token=EAAEry03Wk4EBAPDGUiIQZCsJnzn0RZAJeolzncaP71DQH7ZBuRUUflsLq0QK9j4lTN4YZBotay5J18O1ZCsH6KoGZBE3THom6NrAgwndc3r8yzwak4uK9LXz5sYgHxdako9UZCdm3xiEpQjXax5Ol1KNt1ucms7CEDn1RghhMPRLaRZCX7pzyyXJmjs2X3SvyXiQabZBBHicY0RmLQIPRVH5apsWWEoMEZCCg2RsVEYbfUIQZDZD&since=2018";
-		$calendarData = file_get_contents($facebookevents);
-		die($calendarData);
 		$calendarData = file_get_contents('bootstrap_calendar/events.json');
         echo $calendarData;die();
 	}
