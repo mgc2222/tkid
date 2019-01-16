f949ade776b13b4642ee5de985c3cba931c27fdb
Cristi Miu <cristi.miu@gmail.com>
2019-01-16 18:09:29 +0200

    fixed slider position on moobil by adding 250px

--- js/theme/jquery.prettyPhoto.init.js
+++ js/theme/jquery.prettyPhoto.init.js
@@ -2,7 +2,7 @@
     a(function() {
             var markup =
                 '<div class="pp_pic_holder">' +
-                '   <div class="pp_description">&nbsp;</div>' +
+                '   <div class="ppt hidden" style="margin:0">&nbsp;</div>' +
                 '   <div class="pp_top">' +
                 '      <div class="pp_left"></div>' +
                 '      <div class="pp_middle"></div>' +
@@ -26,7 +26,7 @@
                 '                               <p class="currentTextHolder">0/0</p>' +
                 '                               <a href="#" class="pp_arrow_next">Next</a>' +
                 '                           </div>' +
-                //'                           <p class="ppt"></p>' +
+                '                           <p class="pp_description hidden"></p>' +
                 '                       {pp_social}' +
                 '                       <a class="pp_close" href="#">Close</a>' +
                 '                       </div>' +
@@ -42,16 +42,17 @@
                 '   </div>' +
                 '</div>' +
                 '<div class="pp_overlay"></div>';
-            var gallery_markup =
+            /*var gallery_markup =
                 '<div class="pp_gallery">' +
                 '   <a href="#" class="pp_arrow_previous">Previous</a>' +
                 '   <a href="#" class="pp_arrow_next">Next</a>' +
-                '</div>';
+                '</div>';*/
+
             a("a.zoom").prettyPhoto( {
-                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, gallery_markup: gallery_markup,
+                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, //markup: markup, //gallery_markup: gallery_markup, //show_title: false,
                 }
             ), a("a[data-rel^='prettyPhoto']").prettyPhoto( {
-                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, gallery_markup: gallery_markup, //show_title: false,
+                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, //show_title: false, //gallery_markup: gallery_markup,
                 }
             )
         }
