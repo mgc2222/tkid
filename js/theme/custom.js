36ed6abe3244d8b612402809a0f2220ec6033a59
Cristi Miu <cristi.miu@gmail.com>
2019-01-16 10:58:30 +0200

    prettyPhoto enable gallery slider with custom markup

--- js/theme/custom.js
+++ js/theme/custom.js
@@ -47,11 +47,59 @@ $(document).ready(function($) {
         }
     });
 
-    $(".gallery-item a").prettyPhoto({
+    $(".gallery-item a[rel^='prettyPhoto[gallery]']").prettyPhoto({
         animation_speed:'normal',
-        overlay_gallery: false,
+        //overlay_gallery: true,
         show_title: false,
-        social_tools: false
+        default_width: 'auto',
+        default_height: 'auto',
+        //social_tools: false,
+
+        markup: '<div class="pp_pic_holder">' +
+
+						'<div class="pp_top">' +
+							'<div class="pp_left"></div>' +
+							'<div class="pp_middle"></div>' +
+							'<div class="pp_right"></div>' +
+						'</div>' +
+						'<div class="pp_content_container">' +
+							'<div class="pp_left">' +
+							'<div class="pp_right">' +
+								'<div class="pp_content">' +
+									'<div class="pp_loaderIcon"></div>' +
+									'<div class="pp_fade">' +
+										'<a href="#" class="pp_expand" title="Expand the image">Expand</a>' +
+										'<div class="pp_hoverContainer">' +
+											'<a class="pp_next" href="#">next</a>' +
+											'<a class="pp_previous" href="#">previous</a>' +
+										'</div>' +
+										'<div id="pp_full_res"></div>' +
+										'<div class="pp_details">' +
+											'<div class="pp_nav">' +
+												'<a href="#" class="pp_arrow_previous">Previous</a>' +
+												'<p class="currentTextHolder">0/0</p>' +
+												'<a href="#" class="pp_arrow_next">Next</a>' +
+											'</div>' +
+											'<p class="pp_description"></p>' +
+											'{pp_social}' +
+											'<a class="pp_close" href="#">Close</a>' +
+										'</div>' +
+									'</div>' +
+								'</div>' +
+							'</div>' +
+							'</div>' +
+						'</div>' +
+						'<div class="pp_bottom">' +
+							'<div class="pp_left"></div>' +
+							'<div class="pp_middle"></div>' +
+							'<div class="pp_right"></div>' +
+						'</div>' +
+					'</div>' +
+					'<div class="pp_overlay"></div>',
+        gallery_markup: '<div class="pp_gallery">' +
+								'<a href="#" class="pp_arrow_previous">Previous</a>' +
+								'<a href="#" class="pp_arrow_next">Next</a>' +
+							'</div>',
     });
 
 
