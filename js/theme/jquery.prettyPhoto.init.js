18e679d5ec5a05429c3a2ae7af245885975bb49b
Cristi Miu <cristi.miu@gmail.com>
2019-01-17 12:33:44 +0200

    small change for removing pp_gallery

--- js/theme/jquery.prettyPhoto.init.js
+++ js/theme/jquery.prettyPhoto.init.js
@@ -42,18 +42,18 @@
                 '   </div>' +
                 '</div>' +
                 '<div class="pp_overlay"></div>';
-            /*var gallery_markup =
+            var gallery_markup =
                 '<div class="pp_gallery">' +
                 '   <a href="#" class="pp_arrow_previous">Previous</a>' +
                 '   <a href="#" class="pp_arrow_next">Next</a>' +
-                '</div>';*/
+                '</div>';
 
             a("a.zoom").prettyPhoto( {
-                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, //markup: markup, //gallery_markup: gallery_markup, //show_title: false,
+                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, gallery_markup: gallery_markup, //show_title: false,
                     changepicturecallback: function() {debugger;window.scrollBy(0,1)}
                 }
             ), a("a[data-rel^='prettyPhoto']").prettyPhoto( {
-                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, //show_title: false, //gallery_markup: gallery_markup,
+                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, gallery_markup: gallery_markup, //show_title: false,
                     changepicturecallback: function() {window.scrollBy(0,1);window.scrollBy(0,-1)}
                 }
             )
