aede78b8ae00d477ffd701c06fe05694a52f8e56
Cristi Miu <cristi.miu@gmail.com>
2019-01-17 12:30:34 +0200

    fixed slider position on mobil with callback function

--- js/theme/jquery.prettyPhoto.init.js
+++ js/theme/jquery.prettyPhoto.init.js
@@ -42,17 +42,19 @@
                 '   </div>' +
                 '</div>' +
                 '<div class="pp_overlay"></div>';
-            var gallery_markup =
+            /*var gallery_markup =
                 '<div class="pp_gallery">' +
-                //'   <a href="#" class="pp_arrow_previous">Previous</a>' +
-                //'   <a href="#" class="pp_arrow_next">Next</a>' +
-                '</div>';
+                '   <a href="#" class="pp_arrow_previous">Previous</a>' +
+                '   <a href="#" class="pp_arrow_next">Next</a>' +
+                '</div>';*/
 
             a("a.zoom").prettyPhoto( {
-                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, gallery_markup: gallery_markup, //show_title: false,
+                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, //markup: markup, //gallery_markup: gallery_markup, //show_title: false,
+                    changepicturecallback: function() {debugger;window.scrollBy(0,1)}
                 }
             ), a("a[data-rel^='prettyPhoto']").prettyPhoto( {
-                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup,  gallery_markup: gallery_markup, //show_title: false,
+                    hook: "data-rel", social_tools: !1, theme: "pp_woocommerce", horizontal_padding: 20, opacity: .8, deeplinking: !1, markup: markup, //show_title: false, //gallery_markup: gallery_markup,
+                    changepicturecallback: function() {window.scrollBy(0,1);window.scrollBy(0,-1)}
                 }
             )
         }
